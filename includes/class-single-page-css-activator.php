fdd4f7140963ecdba39a7be90a069bd5cf00ee23
atozsites <admin@atozsites.com>
2020-09-12 22:59:00 +0530

    first commit

---
+++ includes/class-single-page-css-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://atozsites.com
+ * @since      1.0.0
+ *
+ * @package    single_page_css
+ * @subpackage single_page_css/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    single_page_css
+ * @subpackage single_page_css/includes
+ * @author     atozsites <atozsites@gmail.com>
+ */
+class single_page_css_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-single-page-css-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://atozsites.com
+ * @since      1.0.0
+ *
+ * @package    single_page_css
+ * @subpackage single_page_css/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    single_page_css
+ * @subpackage single_page_css/includes
+ * @author     atozsites <atozsites@gmail.com>
+ */
+class single_page_css_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
